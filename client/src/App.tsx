b6ba6a4a4a8553da0aaeb6c876527e5b9f0d04eb
Roman <mirzo1930@gmail.com>
2025-03-19 23:46:42 +0100

    feat: add round preview page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import WaitingList from "./components/WaitingList";
 import ConnectionInput from "./components/ConnectionInput";
 import WaitingPage from "./pages/WaitingPage/WaitingPage";
+import RoundPreview from "./pages/RoundPreview/RoundPreview";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Route path="/" element={<ConnectionInput />} />
 			<Route path="/waitinglist" element={<WaitingList />} />
 			<Route path="/waiting-page" element={<WaitingPage />} />
+			<Route path="/round-preview" element={<RoundPreview />} />
 		</Routes>
 	);
 }
---
+++ client/src/pages/RoundPreview/RoundPreview.tsx
@@ -0,0 +1,21 @@
+import { useSearchParams } from "react-router-dom";
+import styles from "./RoundPreview.module.scss";
+
+export default function RoundPreview() {
+	const [searchParams] = useSearchParams();
+	const round = searchParams.get("round");
+
+	return (
+		<div>
+			<div className={styles.previewBackground}></div>
+			<div className="flex flex-col justify-center items-center w-screen h-screen space-y-4">
+				<h1 className={styles.previewTitle}>РАУНД {round}</h1>
+				<div className={styles.previewDescription}>
+					<p>Обдурив - 500 очок</p>
+					<p>Сказав правду - 1000 очок</p>
+					<p>Ні те, ні се - облизав жабу</p>
+				</div>
+			</div>
+		</div>
+	);
+}
