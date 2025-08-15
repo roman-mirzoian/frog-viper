3f5d1efaa071e505ae4a5a5fe17bf45aefbec051
Roman Mirzoian <r.mirzoian@nethunt.com>
2025-08-15 19:25:33 +0400

    feat: remove client build directory setup from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { app } from "./socket/socket";
-import path from "path";
 import gameRoute from "@/routes/game";
 import adminRouter from "@/routes/admin";
 import userRouter from "@/routes/user";
@@ -20,12 +19,3 @@ app.get('/ping', (_req, res) => {
 app.use('/game', gameRoute);
 app.use('/admin', adminRouter);
 app.use('/users', userRouter);
-
-const currentRootDir = __dirname.split("/").slice(0, -2).join("/");
-const clientBuildDir = path.join(currentRootDir, "/client/dist");
-
-app.use(express.static(clientBuildDir));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(currentRootDir, "client", "dist", "index.html"));
-});
